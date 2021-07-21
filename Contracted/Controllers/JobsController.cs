c36b42d1266782d0968b22dc99a3cef74bde8764
VictorJRochaJR <vrocha897@gmail.com>
2021-07-21 08:56:32 -0600

    Delete Method

--- Contracted/Controllers/JobsController.cs
+++ Contracted/Controllers/JobsController.cs
@@ -67,6 +67,22 @@ namespace Contracted.Controllers
                     return BadRequest(e.Message);
                 }
             }
+              // [Authorize]
+            // [HttpDelete("{id}")]
+            // public async Task<ActionResult<string>> Remove(int id)
+            // {
+            // try
+            // {
+            //     Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+            //    _js.Remove(id, userInfo.Id);
+            //     return Ok("Successfully removed");
+            // }
+            // catch (System.Exception e)
+            // {
+                
+            //    return BadRequest(e.Message);
+            // }
+            // }
 
           
         }
